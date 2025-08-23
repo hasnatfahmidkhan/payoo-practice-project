d9d2bb82579016d887d6b9d7197d60c898939118
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-08-24 00:56:39 +0600

    added resuable function

--- js/features.js
+++ js/features.js
@@ -1,7 +1,3 @@
-const selectBanks = document.querySelector("#select-banks");
-const BankAccountNumber = document.querySelector("#bank-account-number");
-const amountAdd = document.querySelector("#add-amount");
-const pinNumber = document.querySelector("#pin-number");
 const addMoneyBtn = document.querySelector("#add-money-btn");
 const availableBanlace = document.querySelector("#available-balance");
 const addMoneyForm = document.querySelector("#add-money-container");
@@ -11,82 +7,108 @@ const cashOutForm = document.querySelector("#cashout-container");
 const logOutBtn = document.querySelector("#logout-btn");
 const UserPin = 1111;
 
+// Get Element
+function getElement(id) {
+  return document.querySelector(`#${id}`);
+}
+
+// Get Input Field Value
+function getInputValue(id) {
+  const elem = document.querySelector(`#${id}`);
+  return elem.value;
+}
+
+// Valid NUmber checker
+function validNumber(num) {
+  if (num.startsWith("01") && num.length === 11) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 // logout features
 logOutBtn.addEventListener("click", () => {
   window.location.href = "../index.html";
 });
 
-// add Money btn click to open the add money form
-addMoneyFormBtn.addEventListener("click", () => {
-  addMoneyForm.style.display = "initial";
-  cashOutForm.style.display = "none";
-});
-
-// cash out btn click to open the cash out form
-cashOutFormBtn.addEventListener("click", () => {
-  cashOutForm.style.display = "initial";
-  addMoneyForm.style.display = "none";
-});
-
-// Add Money Features
+//* Add Money Features *//
 addMoneyBtn.addEventListener("click", (evt) => {
   evt.preventDefault(); // stop reload after add money btn click
+  // Every click time empty the input field value
+  const accountNotSelected = getElement("not-select-bank");
+  accountNotSelected.textContent = "";
+  const acountNumError = getElement("account-num");
+  acountNumError.textContent = "";
+  const wrongPin = getElement("wrong-pin");
+  wrongPin.textContent = "";
+  const amountNotAdd = getElement("amount-not-add");
+  amountNotAdd.textContent = "";
 
-  document.querySelector("#not-select-bank").textContent = "";
-  document.querySelector("#account-num").textContent = "";
-  document.querySelector("#wrong-pin").textContent = "";
-  document.querySelector("#amount-not-add").textContent = "";
   // Select Bank
-  if (selectBanks.value === "") {
+  const selectBanks = getInputValue("select-banks");
+  if (selectBanks === "") {
     document.querySelector("#not-select-bank").textContent =
       "Please Select A Bank";
     document.querySelector("#not-select-bank").style.display = "initial";
     return;
   }
+
   // Valid Account Number
-  if (BankAccountNumber.value.length < 16) {
-    document.querySelector("#account-num").textContent =
-      "Invalid Account Number";
-    document.querySelector("#account-num").style.display = "initial";
+  const BankAccountNumber = getInputValue("bank-account-number");
+  if (BankAccountNumber.length < 16) {
+    acountNumError.textContent = "Invalid Account Number";
+    acountNumError.style.display = "initial";
     return;
   }
 
   // Ammount
-  if (Number(amountAdd.value) < 5) {
-    document.querySelector("#amount-not-add").textContent = "Add Money";
-    document.querySelector("#amount-not-add").style.display = "initial";
+  const amountAdd = getInputValue("add-amount");
+  if (Number(amountAdd) < 5) {
+    amountNotAdd.textContent = "Add Money";
+    amountNotAdd.style.display = "initial";
     return;
   }
+
   // check pin
-  if (Number(pinNumber.value) !== UserPin) {
-    document.querySelector("#wrong-pin").textContent = "Wrong Password!";
-    document.querySelector("#wrong-pin").style.display = "initial";
+  const pinNumber = getInputValue("pin-number");
+  if (Number(pinNumber) !== UserPin) {
+    wrongPin.textContent = "Wrong Password!";
+    wrongPin.style.display = "initial";
     return;
   }
+
   // after all valid submit add money
-  const addAmount = Number(amountAdd.value);
-  const newAvailableBalance = addAmount + Number(availableBanlace.textContent);
+  const newAvailableBalance =
+    Number(amountAdd) + Number(availableBanlace.textContent);
   availableBanlace.textContent = newAvailableBalance;
+
   // reset the form
   document.querySelector("#add-money-form").reset();
   alert("Add Amount Successfully");
 });
 
-// Cash Out Features
+// Add Money toogle Features
+addMoneyFormBtn.addEventListener("click", () => {
+  addMoneyForm.style.display = "initial";
+  cashOutForm.style.display = "none";
+});
+
+//* Cash Out Features *//
 document
   .querySelector("#withdraw-money-btn")
   .addEventListener("click", (evt) => {
     evt.preventDefault(); // stop reload after add money btn click
 
-    const agentNumber = document.querySelector("#agent-number");
-    const withdrawAmount = document.querySelector("#withdraw-ammount");
-    const pin = document.querySelector("#cashout-pin");
+    const agentNumber = getInputValue("agent-number");
+    const withdrawAmount = getInputValue("withdraw-ammount");
+    const pin = getInputValue("cashout-pin");
     document.querySelector("#agent-wrong-num").textContent = "";
     document.querySelector("#not-add-withdraw").textContent = "";
     document.querySelector("#wrong-pin-cashout").textContent = "";
-
+    const isValidNumber = validNumber(agentNumber);
     // Valid Account Number
-    if (agentNumber.value.length < 11) {
+    if (!isValidNumber) {
       document.querySelector("#agent-wrong-num").textContent =
         "Invalid agent number";
       document.querySelector("#agent-wrong-num").style.display = "initial";
@@ -111,13 +133,13 @@ document
     // after all valid submit add money
     const cashOutAmount = Number(withdrawAmount.value);
     // check cash out is posibble or not
-    console.log(cashOutAmount);
     if (cashOutAmount > Number(availableBanlace.textContent)) {
       document.querySelector("#not-add-withdraw").textContent =
         "Not Available Money";
       document.querySelector("#not-add-withdraw").style.display = "initial";
       return;
     }
+    // New Available Balance 
     const newAvailableBalance =
       Number(availableBanlace.textContent) - cashOutAmount;
     availableBanlace.textContent = newAvailableBalance;
@@ -126,3 +148,8 @@ document
     document.querySelector("#cashout-form").reset();
     alert("Cash Out Successfully");
   });
+// cash out btn click to open the cash out form
+cashOutFormBtn.addEventListener("click", () => {
+  cashOutForm.style.display = "initial";
+  addMoneyForm.style.display = "none";
+});
