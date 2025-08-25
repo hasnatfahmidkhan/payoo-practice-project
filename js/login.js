951f566760a622f093905124bfc5d4a2524977a5
hasnatfahmidkhan <hasnatfahmidkhan@gmail.com>
2025-08-25 13:34:14 +0600

    fix the not showing number error issue

--- js/login.js
+++ js/login.js
@@ -22,7 +22,7 @@ loginBtn.addEventListener("click", (evt) => {
   const userNumber = "01922333444";
   // Valid Account Number
   const isValidNumber = validNumber(mobileNum.value);
-  if (!isValidNumber && !userNumber) {
+  if (!isValidNumber) {
     wrongNum.textContent = "Invalid account number";
     wrongNum.style.display = "initial";
     return;
